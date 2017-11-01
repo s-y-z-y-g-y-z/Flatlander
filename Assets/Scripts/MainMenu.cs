2b00a4f53aab16d4b48ecb1f665ffcd1d9901ccb
Zachary Lopez <ztl25@drexel.edu>
2017-11-01 16:34:14 -0400

    Title Screen
    Quit and Start are working

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public Button start; 
+	public Button lvl;
+	public Button quit;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = start.GetComponent<Button> ();
+		btn.onClick.AddListener (GameStart);
+
+		btn = quit.GetComponent<Button> ();
+		btn.onClick.AddListener (Quit);
+
+		btn = lvl.GetComponent<Button> ();
+		btn.onClick.AddListener (LevelSelect);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	void GameStart()
+	{
+		SceneManager.LoadScene ("proto", LoadSceneMode.Single);
+	}
+
+	void Quit()
+	{
+
+	}
+
+	void LevelSelect()
+	{
+
+	}
+		
+}

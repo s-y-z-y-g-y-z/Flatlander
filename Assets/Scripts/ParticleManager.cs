dd542ba3fc26fad5846ba9223b43eeb59234f8a1
Joshua Karmel <callmeshua620@gmail.com>
2017-11-16 01:02:33 -0500

    jump particles

--- Assets/Scripts/ParticleManager.cs
+++ Assets/Scripts/ParticleManager.cs
@@ -15,26 +15,49 @@ public class ParticleManager : MonoBehaviour {
     public SideScrollController pCtrl;
 
     //Particle References
-    public GameObject jumpSmoke;
+    public GameObject jumpPEs;
+    public GameObject landingPEs;
+    public Vector3 playerVel;
 
 	// Use this for initialization
 	void Start () {
-        jumpSmoke.SetActive(false);
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        handleJumpPEs();
 
-    public void checkJump()
-    {
-        if (Input.GetButtonDown("Jump") && pCtrl.isGrounded){
-            jumpSmoke.SetActive(true);
+        if (!pCtrl.isGrounded) {
+            playerVel = pCtrl.localVelocity;
         }
-        else
+
+        handleLandingPEs();
+    }
+
+    //instantiates the jump particle effects
+    public void handleJumpPEs()
+    {
+        //checks if the player jumps from the ground
+        if (Input.GetButtonDown("Jump") && pCtrl.isGrounded){
+            Transform clone = Instantiate(jumpPEs.transform, pCtrl.transform.position, pCtrl.transform.rotation);
+            jumpPEs.SetActive(true);
+            
+            //clone gameobject is destroyed after .75s
+            Destroy(clone.gameObject,0.75f);
+        }
+    }
+    
+    //Need to figure out how to find velocity when landing
+    public void handleLandingPEs()
+    {
+        //if player falling faster than 5
+        if(playerVel.z < -1.0f && pCtrl.isGrounded)
         {
-            jumpSmoke.SetActive(false);
+            Transform clone = Instantiate(landingPEs.transform, pCtrl.transform.position, pCtrl.transform.rotation);
+            landingPEs.SetActive(true);
+
+            //destroys after .75s
+            Destroy(clone.gameObject, 0.75f);
         }
     }
 }

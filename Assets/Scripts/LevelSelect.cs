44e66e6fe605ac6663a2ccdd047afc33c1d5b01e
Zachary Lopez <ztl25@drexel.edu>
2017-11-16 05:11:31 -0500

    Revert "Particle Effects Manager and Sound manager"
    This reverts commit 54b298bc6dcae9377b9ee9934abb72cfd18c091d.

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -42,13 +42,13 @@ public class LevelSelect : MonoBehaviour {
 	}
 	void Load1()
 	{
-		SceneManager.LoadScene ("BaseScene");
+		SceneManager.LoadScene ("Tutorial");
 	}
 
 	// loads test scene
 	void Load2()
 	{
-		SceneManager.LoadScene ("World4");
+		SceneManager.LoadScene ("Proto2");
 	}
 
     void Load3()
@@ -58,7 +58,7 @@ public class LevelSelect : MonoBehaviour {
 
     void Load4()
     {
-       // SceneManager.LoadScene("World4");
+        SceneManager.LoadScene("World4");
     }
 
 	//go back to main menu screen
